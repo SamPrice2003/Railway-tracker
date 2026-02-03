54fac0c70849e99c92ec5a17628284ebf0c49829
Sufyan <trainee.sufyan.shah@sigmalabs.co.uk>
2026-02-03 16:50:16 +0000

    Working s3_bucket.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,9 +1,11 @@
 ################### VPC ###################
+
 data "aws_vpc" "cohort_vpc" {
   id = var.VPC_ID
 }
 
 ################### Subnets ###################
+
 data "aws_subnets" "public_subnets" {
   filter {
     name   = "vpc-id"
@@ -17,6 +19,7 @@ data "aws_subnets" "public_subnets" {
 }
 
 ################### ECS Cluster ###################
+
 data "aws_ecs_cluster" "c21_cluster" {
   cluster_name = var.CLUSTER_NAME
 }
