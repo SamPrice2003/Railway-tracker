21e5852373b0c79eda7e94cd513596b80f43a462
Enchilada429 <omaryahya.contact@gmail.com>
2026-02-03 12:11:49 +0000

    Turn extracted data into nice json

--- incidents_pipeline/extract.py
+++ incidents_pipeline/extract.py
@@ -4,8 +4,10 @@ from os import environ as ENV, _Environ
 from socket import getfqdn
 from time import sleep
 from logging import getLogger, basicConfig, INFO
-from zlib import decompress, MAX_WBITS
+from json import dumps, dump, loads
+from re import sub
 
+from xmltodict import parse
 from dotenv import load_dotenv
 from stomp import Connection12, ConnectionListener
 from stomp.utils import Frame
@@ -18,48 +20,52 @@ basicConfig(level=INFO)
 
 class Listener(ConnectionListener):
 
-    def get_raw_message_body(self, msg: Frame) -> str:
-        """Returns the raw contents of the message."""
-        msg = decompress(msg.body, MAX_WBITS | 32)
-        return msg
+    def get_clean_message_dict(self, msg: str) -> str:
+        """Cleans message contents according to discrepancies noted on API.
+        Returns a corresponding dictionary."""
+
+        msg = msg.replace(
+            "uk.co.nationalrail.xml.incident.PtIncidentStructure", "PtIncident")
+
+        return loads(sub(r"\/?ns[23]:", "", msg))
 
     def extract_message_details(self, msg: str) -> dict:
         pass
 
     def on_message(self, msg: Frame):
-        """Whenever we receive a message."""
+        """Whenever we receive a message, parse it as a dict and clean the key names."""
         try:
             print(msg.headers)
-            data = self.get_raw_message_body(msg)
+
+            data = self.get_clean_message_dict(dumps(parse(msg.body)))
+
+            with open("data.json", "w") as f:
+                dump(data, f, indent=4)
+
         except Exception as e:
-            logger.error(str(e), type(e))
+            logger.error(str(e))
 
 
 def get_stomp_connection(config: _Environ) -> Connection12:
-    """Returns a STOMP connection to the National Rail Darwin."""
+    """Returns a STOMP connection to the National Rail Real Time Incidents API."""
 
     return Connection12(
         host_and_ports=[(config["STOMP_HOST"], config["STOMP_PORT"])],
         auto_decode=False)
 
 
-def subscribe_connection(config: _Environ, conn: Connection12) -> None:
-    """Subscribes the connection to the the National Rail Darwin topic."""
+def connect_and_subscribe(config: _Environ, conn: Connection12) -> None:
+    """Subscribes the connection to the National Rail Real Time Incidents topic."""
 
     conn.set_listener("", Listener())
 
-    connect_header = {'client-id': config["STOMP_USERNAME"] + '-' + CLIENT_ID}
-    subscribe_header = {'activemq.subscriptionName': CLIENT_ID}
-
     conn.connect(username=config["STOMP_USERNAME"],
                  passcode=config["STOMP_PASSWORD"],
-                 wait=True,
-                 headers=connect_header)
+                 wait=True)
 
     conn.subscribe(destination=f"/topic/{config["STOMP_TOPIC"]}",
-                   id='1',
-                   ack='auto',
-                   headers=subscribe_header)
+                   id="1",
+                   ack="auto")
 
 
 if __name__ == "__main__":
@@ -68,7 +74,7 @@ if __name__ == "__main__":
 
     conn = get_stomp_connection(ENV)
 
-    subscribe_connection(ENV, conn)
+    connect_and_subscribe(ENV, conn)
 
     while True:
         sleep(1)
