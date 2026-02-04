09e2df5ff4dd6e176af7b79bf6017717566f2df9
Sufyan <trainee.sufyan.shah@sigmalabs.co.uk>
2026-02-04 11:28:15 +0000

    Created Listener ECS

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,4 @@
 
 provider "aws" {
   region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,28 +6,56 @@ variable "AWS_ACCOUNT_ID" {
   type = string
 }
 
+variable "CLUSTER_NAME" {
+  type = string
+}
+
 variable "VPC_ID" {
   type = string
 }
 
-variable "CLUSTER_NAME" {
-  type = string
+variable "PUBLIC_SUBNET_IDS" {
+  type = list(string)
 }
 
 variable "S3_BUCKET_NAME" {
   type = string
 }
 
-variable "ECS_IMAGE_URI" {
+
+variable "LISTENER_IMAGE_URI" {
   type = string
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type      = string
-  sensitive = true
+variable "DASHBOARD_IMAGE_URI" {
+  type = string
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
+
+variable "DASHBOARD_PORT" {
+  type    = number
+  default = 8501
+}
+
+# Database
+variable "DB_HOST" {
+  type = string
+}
+
+variable "DB_PORT" {
+  type    = number
+  default = 5432
+}
+
+variable "DB_NAME" {
+  type = string
+}
+
+variable "DB_USERNAME" {
+  type = string
+}
+
+variable "DB_PASSWORD" {
   type      = string
   sensitive = true
 }
