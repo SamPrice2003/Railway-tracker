27e5b671254cec0358699da744d27c869062044d
Sam Price <samprice@Sams-MacBook-Pro.local>
2026-02-04 15:41:32 +0000

    Create database schema

--- database/schema.sql
+++ database/schema.sql
@@ -2,31 +2,55 @@
 
 -- create tables
 
-CREATE TABLE service (
-    service_id INT GENERATED ALWAYS AS IDENTITY,
-    service_uid VARCHAR(6) UNIQUE NOT NULL,
-    origin_station_id INT NOT NULL,
-    destination_station_id INT NOT NULL
-)
-
-CREATE TABLE arrival (
-    arrival_id INT GENERATED ALWAYS AS IDENTITY,
-    scheduled_time TIMESTAMP,
-    actual_time TIMESTAMP,
-    platform_changed BOOLEAN,
-    arrival_station_id INT,
-    service_id INT
-)
-
 CREATE TABLE station (
-    station_id INT GENERATED ALWAYS AS IDENTITY,
+    station_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     station_name VARCHAR UNIQUE NOT NULL,
     station_crs VARCHAR UNIQUE NOT NULL
 )
 
+CREATE TABLE operator (
+    operator_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    operator_name VARCHAR UNIQUE NOT NULL,
+    description VARCHAR
+)
+
+CREATE TABLE service (
+    service_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    service_uid VARCHAR(6) UNIQUE NOT NULL,
+    origin_station_id INT NOT NULL,
+    destination_station_id INT NOT NULL,
+    FOREIGN KEY (origin_station_id) REFERENCES station(station_id) ON DELETE CASCADE,
+    FOREIGN KEY (destination_station_id) REFERENCES station(station_id) ON DELETE CASCADE
+)
+
+CREATE TABLE arrival (
+    arrival_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    scheduled_time TIMESTAMP,
+    actual_time TIMESTAMP,
+    platform_changed BOOLEAN,
+    arrival_station_id INT NOT NULL,
+    service_id INT NOT NULL,
+    FOREIGN KEY (arrival_station_id) REFERENCES station(station_id) ON DELETE CASCADE,
+    FOREIGN KEY (service_id) REFERENCES service(service_id) ON DELETE CASCADE
+)
+
 CREATE TABLE incident (
     incident_id INT GENERATED ALWAYS AS IDENTITY,
-    summary TEXT
+    summary TEXT,
+    incident_start TIMESTAMP NOT NULL,
+    incident_end TIMESTAMP,
+    url VARCHAR,
+    planned BOOLEAN NOT NULL,
+    service_id INT NOT NULL,
+    FOREIGN KEY (service_id) REFERENCES service(service_id) ON DELETE CASCADE
+)
+
+CREATE TABLE incident_assignment (
+    incident_assignment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    incident_id INT NOT NULL,
+    operator_id INT NOT NULL,
+    FOREIGN KEY (incident_id) REFERENCES incident(incident_id) ON DELETE CASCADE,
+    FOREIGN KEY (operator_id) REFERENCES operator(operator_id) ON DELETE CASCADE
 )
 
 
