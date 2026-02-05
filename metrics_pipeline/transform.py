2a0b1a793818255f050871a3ffd1e6c07b9c9df2
Sam Price <samprice@Sams-MacBook-Pro.local>
2026-02-05 14:50:41 +0000

    complete transformation of services and arrivals

--- metrics_pipeline/transform.py
+++ metrics_pipeline/transform.py
@@ -11,6 +11,8 @@ from dotenv import load_dotenv
 
 from extract import extract
 
+logger = getLogger(__name__)
+
 
 def convert_data_to_df(data: list) -> pd.DataFrame:
     """Converts a list of dictionaries to a dataframe."""
@@ -82,13 +84,29 @@ def assign_station_id_to_service(df: pd.DataFrame, station_crs_list: list[dict],
     station_crs_dict = {}
     for entry in station_crs_list:
         station_name = entry["station_name"].replace(" Rail Station", "")
-        station_id = int(entry["station_id"])
+        station_id = entry["station_id"]
         station_crs_dict[station_name] = station_id
 
     if origin:
-        df[column_name] = df["origin_station"].map(station_crs_dict)
+        df[column_name] = df["origin_station"].map(
+            station_crs_dict).astype("Int64")
     else:
-        df[column_name] = df["destination_station"].map(station_crs_dict)
+        df[column_name] = df["destination_station"].map(
+            station_crs_dict).astype("Int64")
+
+    return df
+
+
+def assign_station_id_to_arrival(df: pd.DataFrame, station_crs_list: list[dict]) -> pd.DataFrame:
+    """Assigns the operator_station_id based on the station data in the database."""
+
+    station_crs_dict = {}
+    for entry in station_crs_list:
+        station_crs = entry["station_crs"]
+        station_id = entry["station_id"]
+        station_crs_dict[station_crs] = station_id
+
+    df["arrival_station_id"] = df["crs"].map(station_crs_dict).astype("Int64")
 
     return df
 
@@ -99,54 +117,61 @@ def assign_operator_id_to_service(df: pd.DataFrame, operator_name_list: list[dic
     operator_name_dict = {}
     for entry in operator_name_list:
         operator_name = entry["operator_name"]
-        operator_id = int(entry["operator_id"])
+        operator_id = entry["operator_id"]
         operator_name_dict[operator_name] = operator_id
 
-    df["operator_id"] = df["operator_name"].map(operator_name_dict)
+    df["operator_id"] = df["operator_name"].map(
+        operator_name_dict).astype("Int64")
 
     return df
 
-# FORMAT THE DATA NEEDS TO BE IN FOR SERVICES:
-# service_id (DONE)
-# service_uid (DONE)
-# origin_station_id -
-# destination_station_id -
-# operator_id -
+
+def transform(config: _Environ, data: dict, conn: connection) -> dict:
+    """Returns a dictionary containing the transformed service and arrival data.
+       Apart from service_id in arrival, which needs to be loaded first to access."""
+
+    basicConfig(level=INFO)
+
+    service_df = convert_data_to_df(data["services"])
+    logger.info("Converted services to DataFrame")
+    arrival_df = convert_data_to_df(data["arrivals"])
+    logger.info("Converted arrivals to DataFrame")
+
+    db_station_ids = get_station_id_list(conn=conn)
+    logger.info("Retrieved station ids from RDS")
+
+    service_df = assign_station_id_to_service(
+        service_df, db_station_ids, "origin_station_id", origin=True)
+    service_df = assign_station_id_to_service(
+        service_df, db_station_ids, "destination_station_id", origin=False)
+    logger.info("Assigned station ids to services")
+
+    db_operator_ids = get_operator_id_list(conn=conn)
+    logger.info("Retrieved operator ids from RDS")
+
+    service_df = assign_operator_id_to_service(service_df, db_operator_ids)
+    logger.info("Assigned operator ids to services")
+
+    arrival_df = assign_station_id_to_arrival(arrival_df, db_station_ids)
+    logger.info("Assigned operator ids to arrivals")
+
+    result = {}
+
+    result["services"] = service_df
+    result["arrivals"] = arrival_df
+
+    logger.info("Transformation complete")
+    return result
 
 
-# FORMAT THE DATA NEEDS TO BE IN FOR ARRIVALS:
-# arrival id (DONE since generated)
-# scheduled_time (DONE)
-# actual_time (DONE)
-# platform_changed (DONE)
-# arrival_station_id -
-# service_id -
 if __name__ == "__main__":
 
     load_dotenv()
 
     station_crs_list = ["LBG", "STP", "KGX", "SHF", "LST"]
 
+    data = extract(ENV, station_crs_list=station_crs_list)
+
     conn = get_db_conn(ENV)
 
-    # data = extract(ENV, station_crs_list=station_crs_list)
-
-    # service_df = convert_data_to_df(data["services"])
-    # arrival_df = convert_data_to_df(data["arrivals"])
-
-    service_df = get_data_from_csv_for_test("test_service")
-    arrival_df = get_data_from_csv_for_test("test_arrival")
-
-    db_station_ids = get_station_id_list(conn=conn)
-
-    service_df = assign_station_id_to_service(
-        service_df, db_station_ids, "origin_station_id", origin=True)
-
-    service_df = assign_station_id_to_service(
-        service_df, db_station_ids, "destination_station_id", origin=False)
-
-    db_operator_ids = get_operator_id_list(conn=conn)
-
-    service_df = assign_operator_id_to_service(service_df, db_operator_ids)
-
-    print(service_df[service_df["operator_id"].isna()])
+    DATA = transform(ENV, data, conn)
