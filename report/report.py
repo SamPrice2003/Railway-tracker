605bd87d20df6a4d41873e251d8f57e5f5d5a246
Enchilada429 <omaryahya.contact@gmail.com>
2026-02-05 15:52:57 +0000

    Experiment with PDFs

--- report/report.py
+++ report/report.py
@@ -1,1 +1,23 @@
 """Script for generating a PDF report on the past 24 hours of data."""
+
+from fpdf import FPDF
+
+
+def handler(event=None, context=None):
+    """Lambda function handler for generating PDF reports."""
+
+    pdf = FPDF()
+
+    pdf.add_page()
+
+    pdf.set_font("Arial", size=12)
+
+    pdf.image("../logo/", x=20, y=60)
+
+    pdf.output("output.pdf")
+
+    print("PDF generated successfully!")
+
+
+if __name__ == "__main__":
+    handler()
