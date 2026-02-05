b6f173c2f97fb3c01aa7d17fd80b54702bf8ab8e
Enchilada429 <omaryahya.contact@gmail.com>
2026-02-05 11:00:21 +0000

    Add service assignment table between service and incident in schema

--- database/schema.sql
+++ database/schema.sql
@@ -48,9 +48,7 @@ CREATE TABLE incident (
     incident_start TIMESTAMP NOT NULL,
     incident_end TIMESTAMP,
     url VARCHAR,
-    planned BOOLEAN NOT NULL,
-    service_id INT NOT NULL,
-    FOREIGN KEY (service_id) REFERENCES service(service_id) ON DELETE CASCADE
+    planned BOOLEAN NOT NULL
 );
 
 CREATE TABLE incident_assignment (
@@ -61,6 +59,13 @@ CREATE TABLE incident_assignment (
     FOREIGN KEY (operator_id) REFERENCES operator(operator_id) ON DELETE CASCADE
 );
 
+CREATE TABLE service_assignment (
+    service_assignment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    service_id INT NOT NULL,
+    incident_id INT NOT NULL,
+    FOREIGN KEY (service_id) REFERENCES service(service_id) ON DELETE CASCADE,
+    FOREIGN KEY (incident_id) REFERENCES incident(incident_id) ON DELETE CASCADE
+);
 
 \copy station(station_name, station_crs) from './crs.csv' WITH DELIMITER ',' CSV HEADER;
         
