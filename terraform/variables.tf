2d55ee48a909a38a822e52c495c85dfb8cedea88
Sam Price <samprice@Sams-MacBook-Pro.local>
2026-02-10 11:41:24 +0000

    update environment variables in lambdas and README.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -67,10 +67,18 @@ variable "AWS_SECRET_ACCESS_KEY" {
   sensitive = true
 }
 
+variable "AWS_ECR_REPORT_REPO" {
+  type = string
+}
+
 variable "METRICS_LAMBDA_IMAGE_URI" {
   type = string
 }
 
 variable "REPORTS_LAMBDA_IMAGE_URI" {
+  type = string
+}
+
+variable "SOURCE_EMAIL" {
   type = string
 }
