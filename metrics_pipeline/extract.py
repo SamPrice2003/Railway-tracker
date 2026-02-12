829b0c45fd69f23773da59a0fa148288717af61e
Enchilada429 <omaryahya.contact@gmail.com>
2026-02-12 11:14:08 +0000

    Add testing for extract

--- metrics_pipeline/extract.py
+++ metrics_pipeline/extract.py
@@ -159,7 +159,7 @@ def extract(config: _Environ, station_crs_list: list[str]) -> dict:
     basicConfig(level=INFO)
 
     # get basic authentication for API
-    basic_auth = get_basic_auth(ENV)
+    basic_auth = get_basic_auth(config)
 
     session = requests.Session()
     session.auth = basic_auth
